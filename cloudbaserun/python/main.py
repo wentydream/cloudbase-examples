55783ec976be72fc3610e80c71772e42c901a118
wentydream <48622297+wentydream@users.noreply.github.com>
2021-04-16 09:38:34 +0800

    Add files via upload

--- cloudbaserun/python/main.py
+++ cloudbaserun/python/main.py
@@ -5,8 +5,25 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+def check(request):
+    wechat_data = request.GET
+    signature = wechat_data['signature']
+    timestamp = wechat_data['timestamp']
+    nonce = wechat_data['nonce']
+    echostr = wechat_data['echostr']
+    token = 'gzhtest'
+ 
+    check_list = [token, timestamp, nonce]
+    check_list.sort()
+    s1 = hashlib.sha1()
+    s1.update(''.join(check_list).encode())
+    hashcode = s1.hexdigest()
+    print("handle/GET func: hashcode, signature:{0} {1}".format(hashcode, signature))
+    if hashcode == signature:
+        return HttpResponse(echostr)
+    else:
+        return HttpResponse("")
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)
