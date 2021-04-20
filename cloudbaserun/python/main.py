4ce0fbc6b00aef4a753bc7bcc9277a8f074688d5
wentydream <48622297+wentydream@users.noreply.github.com>
2021-04-20 17:09:17 +0800

    Update main.py

--- cloudbaserun/python/main.py
+++ cloudbaserun/python/main.py
@@ -1,18 +1,15 @@
 import os
 import hashlib
-from flask import request
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 
 @app.route('/')
-# def hello_world():
-#     return 'Hello World!'
-def handle(request):
-    signature = request.get("signature")  # 先获取加密签名
-    timestamp = request.get('timestamp')  # 获取时间戳
-    nonce = request.get("nonece")  # 获取随机数
-    echostr = request.get("echostr") # 获取随机字符串
+def get_request(): # 接受微信发送的GET请求
+    signature = request.args.get("signature")  # 先获取加密签名
+    timestamp = request.args.get('timestamp')  # 获取时间戳
+    nonce = request.args.get("nonce")  # 获取随机数
+    echostr = request.args.get("echostr") # 获取随机字符串
     token = "gzhtest" #自己设置的token
     # 使用字典序排序（按照字母或数字的大小顺序进行排序）
     list = [token, timestamp, nonce]
@@ -21,7 +18,6 @@ def handle(request):
     # 进行sha1加密
     temp = ''.join(list)
     sha1 = hashlib.sha1(temp.encode('utf-8'))
-    # map(sha1.update, list)
     hashcode = sha1.hexdigest()
     # 将加密后的字符串和signatrue对比，如果相同返回echostr,表示验证成功
     if hashcode == signature:
